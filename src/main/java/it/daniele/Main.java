ed9e4b56420584ad3ebfd42af34b63b0ebe9eb95
ferra-rally <danieleferrarelli@gmail.com>
2021-03-24 09:54:12 +0100

    Initial commit

---
+++ src/main/java/it/daniele/Main.java
@@ -0,0 +1,9 @@
+package it.daniele;
+
+import org.json.JSONArray;
+
+public class Main {
+    public static void main(String[] argv) {
+        System.out.println("Hello from maven test");
+    }
+}
